480c18aa587c8b9a569bebdd6e5bade69b45c2e1
bhumitnagda <158726156+bhumitnagda@users.noreply.github.com>
2024-02-03 15:05:49 +0530

    Create welcometojava.java
    The following code is about printing Hello World in Java program hope you find it useful!

---
+++ welcometojava.java
@@ -0,0 +1,11 @@
+//Welcome to Java!
+public class Solution {
+
+    public static void main(String[] args) {
+System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+    }
+}
+
+
+
