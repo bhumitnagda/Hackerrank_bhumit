3a774b12725c14ccf99aa6dcde3d75e0039dc686
bhumitnagda <158726156+bhumitnagda@users.noreply.github.com>
2024-02-03 15:09:03 +0530

    Create javastdinandstdout.java
    This program is all about learning Scanner function in java programming.

---
+++ javastdinandstdout.java
@@ -0,0 +1,21 @@
+//Java Stdin and Stdout I
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        
+        int a = scan.nextInt();
+        System.out.println(a);
+        
+        int b = scan.nextInt();
+        System.out.println(b);
+        
+        int c = scan.nextInt();
+        System.out.println(c);
+    
+        scan.close();
+    }
+}
+
